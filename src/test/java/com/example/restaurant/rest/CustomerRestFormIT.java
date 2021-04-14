173df1c0eeb6eab2e9012d899f0f5ff872f6adee
Konstantinos Psimoulis <kosta@noima.com>
2021-04-14 18:18:18 -0400

    failing unit test

--- src/test/java/com/example/restaurant/rest/CustomerRestFormIT.java
+++ src/test/java/com/example/restaurant/rest/CustomerRestFormIT.java
@@ -43,7 +43,7 @@ public class CustomerRestFormIT {
     @Test
     public void testAInitialCustomerSize() {
         List<Customer> customer = retrieveAllCustomers();
-        assertEquals(customer.size(), 0);
+        assertEquals(customer.size(), 1);
     }
 
     /**
